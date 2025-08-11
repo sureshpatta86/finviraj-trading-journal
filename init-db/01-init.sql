5c1bd861e47cb4ea41bab207940c6651f20d8fcc
sureshpatta86 <sureshnani86@gmail.com>
2025-08-11 15:39:08 +0530

    Migrate project from MySQL to PostgreSQL, update environment variables, and configure Docker for PostgreSQL setup

---
+++ init-db/01-init.sql
@@ -0,0 +1,8 @@
+-- Initial database setup for Trading Journal
+-- This file will be executed when the PostgreSQL container starts for the first time
+
+-- Create additional databases if needed
+-- CREATE DATABASE trading_journal_test;
+
+-- You can add any initial SQL setup here
+SELECT 'Trading Journal PostgreSQL database initialized successfully!' as message;
